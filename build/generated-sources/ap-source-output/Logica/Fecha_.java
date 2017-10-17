d91003bd2c5cb1dc8877a34c4c46b09b0bef459d
Funcu <diego.n.salinas@live.com>
2017-10-17 15:32:19 -0300

    Push provisorio
    Inicio de testeos con JUnit y EclEmma

---
+++ build/generated-sources/ap-source-output/Logica/Fecha_.java
@@ -0,0 +1,16 @@
+package Logica;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-10-17T15:18:32")
+@StaticMetamodel(Fecha.class)
+public class Fecha_ { 
+
+    public static volatile SingularAttribute<Fecha, Integer> mes;
+    public static volatile SingularAttribute<Fecha, Long> id;
+    public static volatile SingularAttribute<Fecha, Integer> dia;
+    public static volatile SingularAttribute<Fecha, Integer> año;
+
+}
---
+++ build/generated-sources/ap-source-output/Logica/Genero_.java
@@ -0,0 +1,22 @@
+package Logica;
+
+import Logica.Album;
+import Logica.Genero;
+import java.util.List;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-10-17T15:18:32")
+@StaticMetamodel(Genero.class)
+public class Genero_ { 
+
+    public static volatile SingularAttribute<Genero, String> padre;
+    public static volatile ListAttribute<Genero, Album> albums;
+    public static volatile ListAttribute<Genero, Genero> hijos;
+    public static volatile SingularAttribute<Genero, List> listas;
+    public static volatile SingularAttribute<Genero, Long> id;
+    public static volatile SingularAttribute<Genero, String> nombre;
+
+}
